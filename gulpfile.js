ca844c1651d4c5a8f3b9f45bc7cd55ba393a0ec9
Celso Marques <celso.marques82@gmail.com>
2015-07-25 12:33:42 -0300

    Uglify

--- gulpfile.js
+++ gulpfile.js
@@ -3,40 +3,50 @@ var path        = require('path')
   , plugins     = require('gulp-load-plugins')()
   , del         = require('del')
   , concat      = require('gulp-concat')
+  , rename      = require('gulp-rename')
   , uglify      = require('gulp-uglify')
   , ngHtml2Js   = require("gulp-ng-html2js")
   , minifyHtml  = require("gulp-minify-html")
   , minifycss   = require("gulp-minify-css");
 
-gulp.task('lint', function() {
-  return gulp.src([ './src/*.js' ])
+gulp.task('lint', function(done) {
+  gulp.src([ './src/*.js' ])
   .pipe(plugins.jshint('.jshintrc'))
   .pipe(plugins.jshint.reporter('jshint-stylish'))
-  .pipe(plugins.jshint.reporter('fail'));
+  .pipe(plugins.jshint.reporter('fail'))
+  .on('end', done);
 });
 
-gulp.task('html2js', function () {
+gulp.task('html2js', function (done) {
   gulp.src([ './src/*.html' ])
   .pipe(minifyHtml())
   .pipe(ngHtml2Js({
     moduleName: "ionic-datepicker.templates"
   }))
   .pipe(concat("templates.js"))
-  // .pipe(uglify())
-  .pipe(gulp.dest("./dist"));
+  .pipe(gulp.dest("./dist"))
+  .pipe(uglify())
+  .pipe(rename({ extname: '.min.js' }))
+  .pipe(gulp.dest("./dist"))
+  .on('end', done);
 });
 
-gulp.task('build', [ 'html2js', 'cssminify' ], function () {
+gulp.task('build', [ 'html2js', 'minify-css' ], function (done) {
   gulp.src(['./src/*.js'])
   // .pipe(uglify())
   .pipe(concat("ionic-datepicker.js"))
-  .pipe(gulp.dest("./dist"));
+  .pipe(gulp.dest("./dist"))
+  .pipe(uglify())
+  .pipe(rename({ extname: '.min.js' }))
+  .pipe(gulp.dest("./dist"))
+  .on('end', done);
 });
 
-gulp.task('cssminify', function () {
-  return gulp.src('./src/*.css')
+gulp.task('minify-css', function (done) {
+  gulp.src('./src/*.css')
   .pipe(minifycss())
-  .pipe(gulp.dest('./dist'));
+  .pipe(gulp.dest('./dist'))
+  .on('end', done);
 });
 
 gulp.task('clean', function () {
