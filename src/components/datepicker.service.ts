46f1d1474b055c5cca16ec5c799d2f47bd77d93e
Askrenteam <renaud.meurisse@yahoo.fr>
2017-07-21 11:00:26 +0200

    Added french(fr-FR) locale

--- src/components/datepicker.service.ts
+++ src/components/datepicker.service.ts
@@ -10,7 +10,7 @@ export class DateService {
     }
     public set locale(val: languages) {
         if (!nls.checkExists(val)) {
-            throw 'Locale not recognized as a valid value. Only en-US/he-IL/ru-RU/pt-BR/de avaliable';
+            throw 'Locale not recognized as a valid value. Only en-US/he-IL/ru-RU/pt-BR/de/fi/fr-FR/zh-CN/zh-TW avaliable';
         }
         DateService._local = val;
     }
